770977f52629935fec032507b2d5edacd4b845e6
aimeryaya <117468082@qq.com>
2017-11-15 12:33:29 +0800

    跨域处理

---
+++ src/main/java/com/wutong/framework/core/web/common/cors/CorsConfiguration.java
@@ -0,0 +1,14 @@
+package com.wutong.framework.core.web.common.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowCredentials(false).maxAge(3600).allowedMethods("GET", "POST");
+    }
+}
