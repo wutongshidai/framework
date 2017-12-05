2104c5da40d8da130ebde960e48f0853fd1d25ea
aimeryaya <117468082@qq.com>
2017-12-05 13:21:59 +0800

    新增支付状态

--- src/main/java/com/wutong/framework/core/web/common/payment/PayStatus.java
+++ src/main/java/com/wutong/framework/core/web/common/payment/PayStatus.java
@@ -1,7 +1,8 @@
 package com.wutong.framework.core.web.common.payment;
 
 public enum PayStatus {
-    UNPAID(10000, "初始订单"), PAID_OFF(10001, "已支付"), NON_PAYMENT(10002, "不可支付");
+    UNPAID(10000, "初始订单"), PAID_OFF(10001, "已支付"), NON_PAYMENT(10002, "不可支付"), PAID_SUCCESS(10003, "支付成功"), PAID_FAIL(10004, "支付失败"), 
+    ORDER_UNEXISTS(10005, "订单不存在");
 
     private int code;
 
